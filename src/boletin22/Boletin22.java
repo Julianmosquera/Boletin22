abd8b34749a258f32d41343e7c4a5a468a95da7a
Julián <Julin@192.168.1.36>
2016-03-04 19:37:34 +0100

    Final

---
+++ src/boletin22/Boletin22.java
@@ -0,0 +1,18 @@
+
+package boletin22;
+
+/**
+ *
+ * @author Julián
+ */
+public class Boletin22 {
+
+   
+    public static void main(String[] args) {
+        Ficheros obx=new Ficheros();   
+        obx.crearArray();
+        obx.crearFichero("Libreria.txt");
+            
+    }
+    
+}
